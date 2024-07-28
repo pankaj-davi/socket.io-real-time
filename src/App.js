83190024f7e2b8f5652da2819df520bf3130bb46
pankaj-davi <menariapankaj17@gmail.com>
2024-07-28 19:22:32 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import socketIOClient from 'socket.io-client';
 import './App.css';
 
+
 function App() {
+  const [testCases, setTestCases] = useState([]);
+
+  useEffect(() => {
+    const socket = socketIOClient(process.env.REACT_BASE_URL);
+    socket.on('FromAPI', (data) => {
+      setTestCases(data);
+    });
+
+    return () => socket.disconnect();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Synechron Real-Time live Test Cases Result POC</h1>
       </header>
+      <main>
+        <ul className="test-cases">
+          {testCases.map((testCase, index) => (
+            <li key={index} className={`test-case ${testCase.status}`}>
+              <h2>{testCase.name}</h2>
+              <p className="status">Status: {testCase.status}</p>
+              <div className="logs">
+                <p>Logs:</p>
+                <ul>
+                  {testCase.logs.map((log, logIndex) => (
+                    <li key={logIndex}>{log}</li>
+                  ))}
+                </ul>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
